50699847216a88d294cd5b053c43843286b65eb0
Ian Lollar <rhv@redhotvengeance.com>
2016-08-08 01:56:05 -0700

    Fix noisy zsh install

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,16 +1,14 @@
 if test ! $(which zsh); then
   echo "Installing zsh..."
   brew install zsh
+
+  echo "Setting zsh as default shell..."
   command -v zsh | sudo tee -a /etc/shells
   chsh -s $(which zsh)
-fi
 
-if test ! $(which zsh-syntax-highlighting); then
   echo "Installing zsh-syntax-highlighting..."
   brew install zsh-syntax-highlighting
-fi
 
-if test ! $(which zsh-autosuggestions); then
   echo "Installing zsh-autosuggestions..."
   brew install zsh-autosuggestions
 fi
