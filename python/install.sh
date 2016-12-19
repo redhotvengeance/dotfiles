74cf895d1756f6a0d4b9a662eacc857f58913185
Ian Lollar <rhv@redhotvengeance.com>
2016-12-18 22:24:41 -0800

    Add required flag before python install

--- python/install.sh
+++ python/install.sh
@@ -1,6 +1,7 @@
 if test ! $(which pyenv); then
   echo "Installing pyenv..."
   brew install pyenv
+  export PYTHON_CONFIGURE_OPTS="--enable-framework"
   pyenv install 2.7.8
   pyenv global 2.7.8
 fi
