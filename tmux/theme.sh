b02710b13ea4314803568499236a8415ab006937
Ian Lollar <rhv@redhotvengeance.com>
2016-12-23 16:32:06 -0800

    Add battery to tmux status line

--- tmux/theme.sh
+++ tmux/theme.sh
@@ -40,10 +40,11 @@ set-option -g display-panes-colour $tm_color_inactive
 set-window-option -g clock-mode-colour $tm_color_active
 
 tm_spotify="#[fg=$tm_color_music]#(osascript ~/.dotfiles/applescripts/spotify.scpt)"
+tm_battery="#[fg=$tm_color_inactive]#(~/.dotfiles/bin/battery)"
 
-tm_date="#[fg=$tm_color_inactive] %R %d %b"
+tm_date="#[fg=$tm_color_inactive]%R %d %b"
 tm_host="#[fg=$tm_color_feature,bold]#h"
 tm_session_name="#[fg=$tm_color_feature,bold]#S"
 
 set -g status-left $tm_session_name'  '
-set -g status-right $tm_spotify'  '$tm_date'  '$tm_host
+set -g status-right $tm_spotify'  '$tm_battery'  '$tm_date'  '$tm_host
