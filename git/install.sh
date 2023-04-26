607d99dff46f9736b6dcacce317dc8b306df8832
Ian Lollar <ian@lollar.email>
2023-04-25 17:58:12 -0700

    Update diff-so-fancy

--- git/install.sh
+++ git/install.sh
@@ -2,3 +2,8 @@ if test ! $(which git); then
   echo "Installing Git..."
   brew install git
 fi
+
+if test ! $(which diff-so-fancy); then
+  echo "Installing diff-so-fancy..."
+  brew install diff-so-fancy
+fi
