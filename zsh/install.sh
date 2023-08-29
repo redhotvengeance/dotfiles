cc3c50f1eeb99ce263ac8503b3f285246b1b0cd6
Ian Lollar <ian@lollar.email>
2023-08-29 10:23:17 -0700

    Use zap for zsh plugins

--- zsh/install.sh
+++ zsh/install.sh
@@ -6,10 +6,9 @@ then
   echo "Setting zsh as default shell..."
   command -v zsh | sudo tee -a /etc/shells
   chsh -s $(which zsh)
+fi
 
-  echo "Installing zsh-syntax-highlighting..."
-  brew install zsh-syntax-highlighting
-
-  echo "Installing zsh-autosuggestions..."
-  brew install zsh-autosuggestions
+if test ! $(which zap); then
+  echo "Installing zap..."
+  zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1 --keep
 fi
---
+++ zsh/plugins.zsh
@@ -0,0 +1,5 @@
+[ -f "${XDG_DATA_HOME:-$HOME/.local/share}/zap/zap.zsh" ] && source "${XDG_DATA_HOME:-$HOME/.local/share}/zap/zap.zsh"
+
+plug "zsh-users/zsh-autosuggestions"
+plug "zap-zsh/supercharge"
+plug "zsh-users/zsh-syntax-highlighting"
