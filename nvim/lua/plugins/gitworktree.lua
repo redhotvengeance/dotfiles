c6b9b20ced688fa0459356000fce6dcb740769f8
Ian Lollar <ian@lollar.email>
2023-08-29 13:45:08 -0700

    Add git worktree nvim plugin

---
+++ nvim/lua/plugins/gitworktree.lua
@@ -0,0 +1,19 @@
+return {
+  "ThePrimeagen/git-worktree.nvim",
+  config = function()
+    require("git-worktree").setup()
+    require("telescope").load_extension('git_worktree')
+  end,
+  keys = {
+    {
+      "<leader>gw",
+      "<CMD>lua require('telescope').extensions.git_worktree.git_worktrees()<CR>",
+      desc = "Show worktrees",
+    },
+    {
+      "<leader>gc",
+      "<CMD>lua require('telescope').extensions.git_worktree.create_git_worktree()<CR>",
+      desc = "Create worktree",
+    },
+  },
+}
