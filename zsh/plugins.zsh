9b80512d4d0dcbb81c3410d3b7773653368523ac
Ian Lollar <ian@lollar.email>
2023-08-29 12:06:30 -0700

    Switch to p10k for zsh prompt

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,5 +1,6 @@
 [ -f "${XDG_DATA_HOME:-$HOME/.local/share}/zap/zap.zsh" ] && source "${XDG_DATA_HOME:-$HOME/.local/share}/zap/zap.zsh"
 
+plug "romkatv/powerlevel10k"
 plug "zsh-users/zsh-autosuggestions"
 plug "zap-zsh/supercharge"
 plug "zsh-users/zsh-syntax-highlighting"
