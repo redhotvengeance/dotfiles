130c60d1f8113438a4d85ecdb13b151eba0a905d
Ian Lollar <ian@lollar.email>
2024-05-16 21:04:19 -0700

    Switch to zinit

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,6 +1,9 @@
-[ -f "${XDG_DATA_HOME:-$HOME/.local/share}/zap/zap.zsh" ] && source "${XDG_DATA_HOME:-$HOME/.local/share}/zap/zap.zsh"
+ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git"
+[ ! -d $ZINIT_HOME ] && mkdir -p "$(dirname $ZINIT_HOME)"
+[ ! -d $ZINIT_HOME/.git ] && git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
+source "${ZINIT_HOME}/zinit.zsh"
 
-plug "zsh-users/zsh-autosuggestions"
-plug "zap-zsh/supercharge"
-plug "zap-zsh/vim"
-plug "zsh-users/zsh-syntax-highlighting"
+zinit light zsh-users/zsh-autosuggestions
+zinit light zap-zsh/supercharge
+zinit light zap-zsh/vim
+zinit light zsh-users/zsh-syntax-highlighting
