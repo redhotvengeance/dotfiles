917e31c2b54a044eb42511cf7a3950316efc2875
Ian Lollar <ian@lollar.email>
2024-07-20 23:04:20 -0700

    Add steam module

---
+++ modules/steam.nix
@@ -0,0 +1,17 @@
+{ pkgs, ... }: {
+  programs.steam = {
+    enable = true;
+
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+    gamescopeSession.enable = true;
+  };
+
+  programs.gamemode.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    protonup
+    mangohud
+  ];
+}
