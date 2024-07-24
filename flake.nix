729527057da8d7243b4196782b02f16458153ace
Ian Lollar <ian@lollar.email>
2024-07-24 15:27:26 -0700

    Add lflinux to hosts

--- flake.nix
+++ flake.nix
@@ -7,10 +7,25 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
 
-  outputs = inputs@{ nixpkgs, home-manager, ... }: {
+  outputs = inputs@{ nixpkgs, home-manager, nixos-hardware, ... }: {
     nixosConfigurations = {
+      lflinux = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+
+        modules = [
+          ./hosts/lflinux/configuration.nix
+          nixos-hardware.nixosModules.framework-13-7040-amd
+
+          home-manager.nixosModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.ian = import ./hosts/lflinux/home.nix;
+          }
+        ];
+      };
       vali = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
 
---
+++ hosts/lflinux/home.nix
@@ -0,0 +1,52 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ../../home/bat
+    ../../home/brave
+    ../../home/eza
+    ../../home/foot
+    ../../home/fzf
+    ../../home/git
+    ../../home/gmailctl
+    ../../home/hypridle
+    ../../home/hyprland
+    ../../home/hyprlock
+    ../../home/json
+    ../../home/lf
+    ../../home/nvim
+    ../../home/pass
+    ../../home/rg
+    ../../home/rofi
+    ../../home/slack
+    ../../home/swaync
+    ../../home/tmux
+    ../../home/waybar
+    ../../home/xh
+    ../../home/zsh
+  ];
+
+  home.username = "ian";
+  home.homeDirectory = "/home/ian";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "24.05"; # Please read the comment before changing.
+
+  home.packages = [
+  ];
+
+  home.file = {
+  };
+
+  home.sessionVariables = {
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
