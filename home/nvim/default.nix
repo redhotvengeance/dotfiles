fe9562341695eb5363ac6b804739b074740fed96
Ian Lollar <ian@lollar.email>
2024-07-27 11:19:24 -0700

    Update nvim lsp config
    Remove mason and handle server install via nix.

--- home/nvim/default.nix
+++ home/nvim/default.nix
@@ -6,6 +6,21 @@
 
   home.packages = with pkgs; [
     gcc
+    gnumake
+
+    # language servers
+    nodePackages_latest.bash-language-server
+    gopls
+    lua-language-server
+    marksman
+    pyright
+    sqls
+    stylua
+    tailwindcss-language-server
+    templ
+    nodePackages_latest.typescript-language-server
+    yaml-language-server
+    vscode-langservers-extracted
   ];
 
   home.file.".config/nvim".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/home/nvim";
--- home/nvim/lua/plugins/lsp.lua
+++ home/nvim/lua/plugins/lsp.lua
@@ -1,21 +1,16 @@
 return {
   "neovim/nvim-lspconfig",
-  lazy = true,
-  cmd = "LspInfo",
-  event = { "BufReadPre", "BufNewFile" },
   dependencies = {
-    {
-      "williamboman/mason.nvim",
-      config = true
-    },
-    "williamboman/mason-lspconfig.nvim",
-    {
-      "j-hui/fidget.nvim",
-      tag = "legacy",
-      opts = {}
-    },
+    { "j-hui/fidget.nvim", opts = {} },
   },
   config = function()
+    local capabilities = nil
+    local lspconfig = require("lspconfig")
+
+    if pcall(require, "cmp_nvim_lsp") then
+      capabilities = require("cmp_nvim_lsp").default_capabilities()
+    end
+
     local servers = {
       bashls = {},
       cssls = {},
@@ -23,71 +18,68 @@ return {
       html = {},
       jsonls = {},
       lua_ls = {
-        Lua = {
-          workspace = { checkThirdParty = false },
-          telemetry = { enable = false },
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = { "vim" },
+            },
+            telemetry = { enable = false },
+            workspace = { checkThirdParty = false },
+          },
+        },
+        server_capabilities = {
+          semanticTokensProvider = nil,
         },
       },
       marksman = {},
-      pylsp = {},
+      pyright = {},
       sqlls = {},
-      tsserver = {},
+      tailwindcss = {},
+      templ = {},
+      tsserver = {
+        server_capabilities = {
+          documentFormattingProvider = false,
+        },
+      },
       yamlls = {},
     }
 
-    local capabilities = vim.lsp.protocol.make_client_capabilities()
-    capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+    local on_attach = function(client, bufnr)
+      local builtin = require("telescope.builtin")
+      local settings = servers[client.name]
 
-    local mason_lspconfig = require("mason-lspconfig")
+      vim.opt_local.omnifunc = "v:lua.vim.lsp.omnifunc"
 
-    mason_lspconfig.setup({
-      ensure_installed = vim.tbl_keys(servers),
-    })
-
-    mason_lspconfig.setup_handlers({
-      function(server_name)
-        require("lspconfig")[server_name].setup({
-          capabilities = capabilities,
-          on_attach = function(_, bufnr)
-            local opts = function(desc)
-              if desc then
-                desc = "LSP: " .. desc
-              end
-
-              return { buffer = bufnr, desc = desc }
-            end
-
-            vim.bo[bufnr].omnifunc = "v:lua.vim.lsp.omnifunc"
-
-            vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts("Goto definition"))
-            vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts("Goto declaration"))
-            vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts("Goto implementation"))
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, opts("Hover documentation"))
-            vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts("Signature documentation"))
-            vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts("Add workspace folder"))
-            vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts("Remove workspace folder"))
-            vim.keymap.set("n", "<leader>wl", function()
-              print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-            end, opts("List workspace folders"))
-            vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts("Type definition"))
-            vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts("Rename"))
-            vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts("Code action"))
-            vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references, opts("Goto references"))
-            vim.keymap.set("n", "<leader>f", function()
-              vim.lsp.buf.format { async = true }
-            end, opts("Format file"))
-            vim.keymap.set("n", "<leader>ds", require("telescope.builtin").lsp_document_symbols, opts("Document symbols"))
-            vim.keymap.set("n", "<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols,
-              opts("Workspace symbols"))
-
-            vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
-              vim.lsp.buf.format()
-            end, { desc = "Format current buffer with LSP" })
-          end,
-          settings = servers[server_name],
-          filetypes = (servers[server_name] or {}).filetypes,
-        })
+      local map = function(keys, func, desc)
+        vim.keymap.set("n", keys, func, { desc = "LSP: " .. desc, buffer = bufnr, noremap = true, silent = true })
       end
-    })
-  end
+
+      map("gd", builtin.lsp_definitions, "[g]oto [d]efinition")
+      map("gD", vim.lsp.buf.declaration, "[g]oto [D]eclaration")
+      map("gr", builtin.lsp_references, "[g]oto [r]eferences")
+      map("gI", builtin.lsp_implementations, "[g]oto [I]mplementation")
+      map("K", vim.lsp.buf.hover, "Hover documentation")
+      map("<leader>D", builtin.lsp_type_definitions, "Type [D]efinition")
+      map("<leader>ds", builtin.lsp_document_symbols, "[d]ocument [s]ymbols")
+      map("<leader>ws", builtin.lsp_dynamic_workspace_symbols, "[w]orkspace [s]ymbols")
+      map("<leader>rn", vim.lsp.buf.rename, "[r]e[n]ame")
+      map("<leader>ca", vim.lsp.buf.code_action, "[c]ode [a]ction")
+
+      if settings.server_capabilities then
+        for k, v in pairs(settings.server_capabilities) do
+          client.server_capabilities[k] = v
+        end
+      end
+    end
+
+    for name, config in pairs(servers) do
+      config = vim.tbl_deep_extend("force", {}, {
+        capabilities = capabilities,
+      }, config)
+
+      config.on_attach = on_attach
+
+      lspconfig[name].setup(config)
+    end
+  end,
 }
