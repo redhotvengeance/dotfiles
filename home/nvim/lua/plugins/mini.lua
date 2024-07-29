335cfb023e414933131ba69c065017ac12099b06
Ian Lollar <ian@lollar.email>
2024-07-29 11:44:35 -0700

    Add mini.surround

--- home/nvim/lua/plugins/mini.lua
+++ home/nvim/lua/plugins/mini.lua
@@ -7,4 +7,12 @@ return {
 			require("mini.ai").setup()
 		end,
 	},
+	{
+		"echasnovski/mini.surround",
+		version = false,
+		event = "VeryLazy",
+		config = function()
+			require("mini.surround").setup()
+		end,
+	},
 }
