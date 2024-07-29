a53c35fb23d389f0a76851142ce957454dabf8db
Ian Lollar <ian@lollar.email>
2024-07-29 16:30:17 -0700

    Update to harpoon2

--- home/nvim/lua/plugins/harpoon.lua
+++ home/nvim/lua/plugins/harpoon.lua
@@ -1,46 +1,51 @@
 return {
 	"ThePrimeagen/harpoon",
+	branch = "harpoon2",
 	lazy = true,
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
 	keys = {
 		{
 			"<leader>a",
 			function()
-				require("harpoon.mark").add_file()
+				require("harpoon"):list():add()
 			end,
 			desc = "Add file to Harpoon",
 		},
 		{
-			"<C-h>",
+			"<leader>h",
 			function()
-				require("harpoon.ui").toggle_quick_menu()
+				local harpoon = require("harpoon")
+				harpoon.ui:toggle_quick_menu(harpoon:list())
 			end,
 			desc = "Toggle Harpoon",
 		},
 		{
 			"<C-1>",
 			function()
-				require("harpoon.ui").nav_file(1)
+				require("harpoon"):list():select(1)
 			end,
 			desc = "Switch to first Harpoon mark",
 		},
 		{
 			"<C-2>",
 			function()
-				require("harpoon.ui").nav_file(2)
+				require("harpoon"):list():select(2)
 			end,
 			desc = "Switch to second Harpoon mark",
 		},
 		{
 			"<C-3>",
 			function()
-				require("harpoon.ui").nav_file(3)
+				require("harpoon"):list():select(3)
 			end,
 			desc = "Switch to third Harpoon mark",
 		},
 		{
 			"<C-4>",
 			function()
-				require("harpoon.ui").nav_file(4)
+				require("harpoon"):list():select(4)
 			end,
 			desc = "Switch to fourth Harpoon mark",
 		},
