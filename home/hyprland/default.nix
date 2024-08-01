2ce20f458be071bba206c811aeb9313b5bc61e98
Ian Lollar <ian@lollar.email>
2024-08-01 12:27:57 -0700

    Expand logic for hyprland clamshell mode
    Also move clamshell mode logic into Nix-generated shell script.

--- home/hyprland/default.nix
+++ home/hyprland/default.nix
@@ -1,12 +1,38 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+let
+  clamshell = pkgs.writeShellScript "clamshell" ''
+    #!/usr/bin/env bash
+
+    sleep 1
+
+    LID_OPEN=$(${pkgs.gnugrep}/bin/grep -c open /proc/acpi/button/lid/LID0/state)
+    NUM_MONITORS=$(hyprctl monitors all | ${pkgs.gnugrep}/bin/grep -c Monitor)
+
+    if [[ $1 == "on" ]]; then
+      if [[ $NUM_MONITORS -gt 1 ]]; then
+        if [[ $LID_OPEN == 0 ]]; then
+          hyprctl keyword monitor "eDP-1,disable"
+        fi
+      fi
+    elif [[ $1 == "off" ]]; then
+      if [[ $LID_OPEN == 1 ]]; then
+        hyprctl keyword monitor "eDP-1,preferred,auto,1.566667"
+      fi
+    fi
+  '';
+in {
   wayland.windowManager.hyprland = {
     enable = true;
 
-    extraConfig = builtins.readFile ./hyprland.conf;
+    extraConfig = builtins.readFile ./hyprland.conf + ''
+      bindl =, switch:on:Lid Switch, exec, ${clamshell} on
+      bindl =, switch:off:Lid Switch, exec, ${clamshell} off
+    '';
   };
 
   home.packages = with pkgs; [
     lxqt.lxqt-policykit
+    gnugrep
   ];
 
   xdg.portal = {
