31d85b396c4e8348395f7677f0dad9ffa91e5e14
Ian Lollar <ian@lollar.email>
2024-08-03 15:38:15 -0700

    Update nvim options

--- home/nvim/lua/config/options.lua
+++ home/nvim/lua/config/options.lua
@@ -2,33 +2,39 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-vim.opt.termguicolors = true -- make pretty colors
-vim.opt.number = true -- line numbers are kinda important
-vim.opt.relativenumber = true -- count is slow
-vim.opt.smartindent = true -- indent my stuff
-vim.opt.wrap = false -- line wrapping is bad
-vim.opt.termguicolors = true -- colors are good
-vim.opt.updatetime = 50 -- fast is fast
 vim.opt.colorcolumn = "80" -- show textwidth guide
-
--- spaces spaces spaces
-vim.opt.expandtab = true
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.shiftround = true
-
--- keep a buffer of 8 lines as i scroll
-vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
-vim.opt.isfname:append("@-@")
-
--- case stuff while searching
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-
--- make my undos go back a long time
-vim.opt.swapfile = false
-vim.opt.backup = false
+vim.opt.cursorline = true -- help me find my cursor
+vim.opt.expandtab = true -- spaces over tabs
+vim.opt.ignorecase = true -- big == small
+vim.opt.jumpoptions = "view"
+vim.opt.laststatus = 3 -- global statusline
+vim.opt.linebreak = true -- wrap lines at better points
+vim.opt.list = true -- show those sneaky invisible chars
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+vim.opt.mouse = "a" -- mouse? ew...but okay, maybe sometimes...
+vim.opt.number = true -- line numbers are kinda important
+vim.opt.pumblend = 10 -- make popup semi-transparent
+vim.opt.pumheight = 15 -- maximum number of entries in a popup
+vim.opt.relativenumber = true -- counting is slow
+vim.opt.scrolloff = 10 -- lines of context
+vim.opt.shiftround = true -- round the indent
+vim.opt.shiftwidth = 2 -- indent by 2 spaces
+vim.opt.showmode = false -- no need when lualine always shows it
+vim.opt.sidescrolloff = 10 -- columns of context
+vim.opt.signcolumn = "yes" -- always show the signs
+vim.opt.smartcase = true -- only care about case when searching with case
+vim.opt.smartindent = true -- indent my stuff for me
+vim.opt.softtabstop = 2 -- tab equals 2 spaces
+vim.opt.splitbelow = true -- always horizontal split below
+vim.opt.splitkeep = "screen"
+vim.opt.splitright = true -- always vertical split right
+vim.opt.tabstop = 2 -- tab equals 2 spaces
+vim.opt.termguicolors = true -- colors are good
+vim.opt.timeoutlen = 300 -- quicken key triggers
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
+vim.opt.undolevels = 10000 -- give me all that undo
+vim.opt.updatetime = 200 -- fast is fast
+vim.opt.virtualedit = "block" -- allow cursor to move to where there is no text in visual block mode
+vim.opt.wildmode = "longest:full,full" -- command-line completion mode
+vim.opt.wrap = false -- line wrapping is bad
