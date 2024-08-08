9933a5f494d8d5cdcd327ee0203b1169b5cbcff8
Ian Lollar <ian@lollar.email>
2024-08-08 11:44:50 -0700

    Update mini.surround keymaps to use <C-s>
    Stops the annoying take over of the default s key vim behavior.
    Apparently I'm in the minority of users that likes to use that key?

--- home/nvim/lua/plugins/mini.lua
+++ home/nvim/lua/plugins/mini.lua
@@ -11,8 +11,19 @@ return {
 		"echasnovski/mini.surround",
 		version = false,
 		event = "VeryLazy",
-		config = function()
-			require("mini.surround").setup()
-		end,
+		opts = {
+			mappings = {
+				add = "<C-s>a", -- Add surrounding in Normal and Visual modes
+				delete = "<C-s>d", -- Delete surrounding
+				find = "<C-s>f", -- Find surrounding (to the right)
+				find_left = "<C-s>F", -- Find surrounding (to the left)
+				highlight = "<C-s>h", -- Highlight surrounding
+				replace = "<C-s>r", -- Replace surrounding
+				update_n_lines = "<C-s>n", -- Update `n_lines`
+
+				suffix_last = "l", -- Suffix to search with "prev" method
+				suffix_next = "n", -- Suffix to search with "next" method
+			},
+		},
 	},
 }
