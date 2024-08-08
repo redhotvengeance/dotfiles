db6553813d439e20df1aeb01e0c3b411448f3863
Ian Lollar <ian@lollar.email>
2024-08-07 23:48:34 -0700

    Update which-key.nvim config and version

--- home/nvim/lua/plugins/whichkey.lua
+++ home/nvim/lua/plugins/whichkey.lua
@@ -1,32 +1,22 @@
 return {
-	{
-		"folke/which-key.nvim",
-		event = "VeryLazy",
-		opts = {
-			plugins = { spelling = true },
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	opts = {
+		preset = "modern",
+		triggers = {},
+		keys = {
+			scroll_down = "<m-d>", -- binding to scroll down inside the popup
+			scroll_up = "<m-u>", -- binding to scroll up inside the popup
 		},
-		config = function(_, opts)
-			local wk = require("which-key")
-			wk.setup(opts)
-			local keymaps = {
-				mode = { "n", "v" },
-				["g"] = { name = "+goto" },
-				["gz"] = { name = "+surround" },
-				["]"] = { name = "+next" },
-				["["] = { name = "+prev" },
-				["<leader><tab>"] = { name = "+tabs" },
-				["<leader>b"] = { name = "+buffer" },
-				["<leader>c"] = { name = "+code" },
-				["<leader>f"] = { name = "+file/find" },
-				["<leader>g"] = { name = "+git" },
-				["<leader>gh"] = { name = "+hunks" },
-				["<leader>q"] = { name = "+quit/session" },
-				["<leader>s"] = { name = "+search" },
-				["<leader>u"] = { name = "+ui" },
-				["<leader>w"] = { name = "+windows" },
-				["<leader>x"] = { name = "+diagnostics/quickfix" },
-			}
-			wk.register(keymaps)
-		end,
+		show_help = false,
+	},
+	keys = {
+		{
+			"<leader>wk",
+			function()
+				require("which-key").show({ global = true })
+			end,
+			desc = "Show [w]hich[k]ey",
+		},
 	},
 }
