f5d697be7a04670e0ae5b740830c1839403410d8
Ian Lollar <ian@lollar.email>
2024-09-04 12:59:10 -0700

    There's a foot.ini, so...use it?

--- home/foot/default.nix
+++ home/foot/default.nix
@@ -1,5 +1,7 @@
-{ pkgs, ... }: {
+{ config, pkgs, dotfiles, ... }: {
   home.packages = with pkgs; [
     foot
   ];
+
+  home.file.".config/foot/foot.ini".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/home/foot/foot.ini";
 }
