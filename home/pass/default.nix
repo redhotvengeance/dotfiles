4a446ed715975611c320d7a3e9316f7e48795332
Ian Lollar <ian@lollar.email>
2024-10-15 16:59:11 -0700

    Update pass config

--- home/pass/default.nix
+++ home/pass/default.nix
@@ -1,3 +1,15 @@
-{ pkgs, ... }: {
-  programs.password-store.enable = true;
+{ pkgs, config, ... }: {
+  programs.password-store = {
+    enable = true;
+
+    settings = {
+      PASSWORD_STORE_DIR = "${config.home.homeDirectory}/.password-store";
+    };
+  };
+
+  programs.browserpass.enable = true;
+
+  home.packages = with pkgs; [
+    asc-key-to-qr-code-gif
+  ];
 }
