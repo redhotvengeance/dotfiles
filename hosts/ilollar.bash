12883452f3a9bb9919183cffedf88b9b22a35090
Ian Lollar <ian@lollar.email>
2025-07-30 14:15:41 -0700

    Add ilollar host

---
+++ hosts/ilollar.bash
@@ -0,0 +1,18 @@
+export SYSTEM="glinux"
+
+export PACKAGES=(
+  nix # start with nix so it's available to the other packages
+
+  bat
+  direnv
+  eza
+  fzf
+  git
+  json
+  less
+  neovim
+  ripgrep
+  tmux
+  xh
+  zsh
+)
