58addb6e2a2cfbd43dc9188039002249d595d22c
Ian Lollar <ian@lollar.email>
2025-07-30 14:12:36 -0700

    Add nix config files and symlinks

--- home/nix/link.bash
+++ home/nix/link.bash
@@ -1,3 +1,7 @@
 mkdir -p "$HOME/.config/fontconfig/conf.d/"
+mkdir -p "$HOME/.config/nix"
+mkdir -p "$HOME/.config/nixpkgs"
 
 link "$HOME_DIR/nix/10-nix-fonts.conf" "$HOME/.config/fontconfig/conf.d/10-nix-fonts.conf"
+link "$HOME_DIR/nix/config.nix" "$HOME/.config/nixpkgs/config.nix"
+link "$HOME_DIR/nix/nix.conf" "$HOME/.config/nix/nix.conf"
