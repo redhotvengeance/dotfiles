f26db41ff758e4c51d00d31e18ea02224fef2adb
Ian Lollar <ian@lollar.email>
2025-12-20 20:44:59 -0800

    Add desktop files for chromium and vesktop

---
+++ home/chromium/link.bash
@@ -0,0 +1,3 @@
+mkdir -p "$HOME/.local/share/applications"
+
+link "$HOME_DIR/chromium/chromium.desktop" "$HOME/.local/share/applications/chromium.desktop"
---
+++ home/discord/link.bash
@@ -0,0 +1,3 @@
+mkdir -p "$HOME/.local/share/applications"
+
+link "$HOME_DIR/discord/vesktop.desktop" "$HOME/.local/share/applications/vesktop.desktop"
