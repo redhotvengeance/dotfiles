f99d7b5ec4b3f7c4cd9efe55abee8d925de16b85
Ian Lollar <ian@lollar.email>
2026-03-11 23:12:50 -0700

    Add matugen fallback files

--- home/matugen/link.bash
+++ home/matugen/link.bash
@@ -1,5 +1,10 @@
 if [[ "$SYSTEM" == "arch" || "$SYSTEM" == "cachy" ]]; then
   mkdir -p "$HOME/.config/matugen"
+  mkdir -p "$HOME/.cache/matugen"
 
   link "$HOME_DIR/matugen/config.toml" "$HOME/.config/matugen/config.toml"
+
+  touch "$HOME/.cache/matugen/ghostty"
+  touch "$HOME/.cache/matugen/hyprland-colors.conf"
+  touch "$HOME/.cache/matugen/waybar.css"
 fi
