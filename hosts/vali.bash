18660850d7b2cd4ba4ddd6c8da6ed314f9c96234
Ian Lollar <ian@lollar.email>
2026-03-11 23:44:36 -0700

    Update vali host

--- hosts/vali.bash
+++ hosts/vali.bash
@@ -1,5 +1,36 @@
-export SYSTEM="arch"
+export GPU="nvidia"
+export SYSTEM="cachy"
 
 export PACKAGES=(
+  nix # start with nix so it's available to the other packages
+  paru # start with paru so it's available to the other packages
+
+  bat
+  discord
+  eza
+  fzf
+  ghostty
+  git
+  hypridle
+  hyprland
+  hyprlock
+  json
   less
+  localsend
+  mako
+  matugen
+  neovim
+  qutebrowser
+  ripgrep
+  steam
+  swww
+  tailscale
+  tldr
+  tmux
+  tofi
+  uwsm
+  waybar
+  xh
+  yazi
+  zsh
 )
