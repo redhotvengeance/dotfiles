80183c03124bda818fb6aeb09a4fddcfd012c032
Ian Lollar <ian@lollar.email>
2026-03-11 22:28:38 -0700

    Update uwsm to support multiple architectures

--- home/uwsm/link.bash
+++ home/uwsm/link.bash
@@ -1,5 +1,15 @@
 if [[ "$SYSTEM" == "arch" || "$SYSTEM" == "cachy" ]]; then
   mkdir -p "$HOME/.config/uwsm"
+  mkdir -p "$HOME/.config/uwsm/env.d"
 
-  link "$HOME_DIR/uwsm/env" "$HOME/.config/uwsm/env"
+  link "$HOME_DIR/uwsm/env.base" "$HOME/.config/uwsm/env"
+  link "$HOME_DIR/uwsm/env.hyprland" "$HOME/.config/uwsm/env-hyprland"
+
+  if [[ "$GPU" == "amd" ]]; then
+    link "$HOME_DIR/uwsm/env.amd" "$HOME/.config/uwsm/env.d/90-hardware"
+  fi
+
+  if [[ "$GPU" == "nvidia" ]]; then
+    link "$HOME_DIR/uwsm/env.nvidia" "$HOME/.config/uwsm/env.d/90-hardware"
+  fi
 fi
