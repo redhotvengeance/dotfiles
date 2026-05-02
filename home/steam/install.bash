25787ced3a44f0b7e8171eaf51a30680939b4e5b
Ian Lollar <ian@lollar.email>
2026-05-02 13:05:23 -0700

    Update steam install for arch systems
    If on arch, make sure that pacman has multilib enabled before attempting
    to install Steam.

--- home/steam/install.bash
+++ home/steam/install.bash
@@ -1,6 +1,20 @@
 # shellcheck source=../../script/packages.bash
 source "$SCRIPT_DIR/packages.bash"
 
+if [[ "$SYSTEM" == "arch" ]]; then
+  if ! grep -q "^\[multilib\]" /etc/pacman.conf; then
+    echo "enabling multilib..."
+    $cmd sudo sed -i '/^#\[multilib\]/,/^#Include/ s/^#//' /etc/pacman.conf
+
+    $cmd sudo pacman -Sy
+  else
+    echo "multilib is already enabled"
+  fi
+
+  pacman gamescope goverlay mangohud
+  pacman steam
+fi
+
 if [[ "$SYSTEM" == "cachy" ]]; then
   pacman cachyos-gaming-meta
   pacman gamescope goverlay mangohud
