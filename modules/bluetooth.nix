5af5d6cf505dd1af8b4c25bf4d6c171c89e68008
Ian Lollar <ian@lollar.email>
2026-06-13 20:11:28 -0700

    Remove legacy nixos stuff

--- modules/bluetooth.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs, ... }: {
-  hardware.bluetooth.enable = true;
-  hardware.bluetooth.powerOnBoot = false;
-
-  environment.systemPackages = with pkgs; [
-    bluetuith
-  ];
-}
--- modules/wayland.nix
+++
@@ -1,7 +0,0 @@
-{ pkgs, ... }: {
-  environment.systemPackages = with pkgs; [
-    wl-clipboard
-  ];
-
-  environment.sessionVariables.NIXOS_OZONE_WL = "1";
-}
