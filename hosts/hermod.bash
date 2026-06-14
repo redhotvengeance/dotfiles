a9fb7f8fadb3371f5bae79868bbeb3b0e0879e0e
Ian Lollar <ian@lollar.email>
2026-06-13 17:15:17 -0700

    Add hermod host

---
+++ hosts/hermod.bash
@@ -0,0 +1,23 @@
+export SYSTEM="macos"
+
+export PACKAGES=(
+  brew # start with homebrew so it's available to the other packages
+
+  bat
+  direnv
+  eza
+  fzf
+  ghostty
+  git
+  json
+  less
+  localsend
+  neovim
+  ripgrep
+  tldr
+  tmux
+  vivaldi
+  xh
+  yazi
+  zsh
+)
