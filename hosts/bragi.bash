5b7c507167eb9e0f8960e39bd50055f04ba15702
Ian Lollar <ian@lollar.email>
2026-06-13 19:58:34 -0700

    Add bragi host

---
+++ hosts/bragi.bash
@@ -0,0 +1,24 @@
+export SYSTEM="macos"
+
+export PACKAGES=(
+  brew # start with homebrew so it's available to the other packages
+
+  bat
+  direnv
+  eza
+  fzf
+  ghostty
+  git
+  json
+  less
+  localsend
+  neovim
+  obs
+  ripgrep
+  tldr
+  tmux
+  vivaldi
+  xh
+  yazi
+  zsh
+)
